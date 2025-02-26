f2520855180947ce543e0e4d678b49b4a5951ae0
dbym4820 <aburatanitomoki@gmail.com>
2025-02-26 11:11:06 +0900

    add pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,5 +13,19 @@ const products = defineCollection({
   	}),
 });
 
-export const collections = { products };
+const projects = defineCollection({
+ 	type: 'content',
+ 	// Type-check frontmatter using a schema
+ 	schema: z.object({
+ 		title: z.string(),
+ 		description: z.string(),
+  		// Transform string to Date object
+  		heroImage: z.string().optional(),
+        systemUrl: z.string().optional(),
+        projectType: z.string(),
+        work_list: z.array(z.string()).optional(),
+  	}),
+});
+
+export const collections = { products, projects };
 
